311e89dd5c11b3c50a97f02accaa898650244d3d
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-25 15:08:44 +0500

    task 11.24 done

--- enumerated_data_types/solutions.cpp
+++ enumerated_data_types/solutions.cpp
@@ -1,13 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-enum Flower { ROSE, DAISY, PETUNIA };
+enum Flower
+{
+    ROSE,
+    DAISY,
+    PETUNIA
+};
 // it will display 0,1,2 in memory
 
-enum { HOBBIT, ELF = 7, DRAGON };
+enum
+{
+    HOBBIT,
+    ELF = 7,
+    DRAGON
+};
 // 11.23--> it is anonymous enum type
 
-int main(){
+enum Letters
+{
+    Z,
+    Y,
+    X
+};
+int main()
+{
     // cout<<ROSE;
     cout << HOBBIT << " " << ELF << " " << DRAGON << endl;
     /*
@@ -16,5 +33,9 @@ int main(){
     ELF-->7
     DRAGON-->0
     */
+    if (Z > X)
+        cout << "Z is greater than X. \n"; // this will not be printed
+    else
+        cout << "Z is not greater than X. \n"; // this will be printed
     return 0;
 }

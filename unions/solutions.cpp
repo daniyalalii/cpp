ec285550e08f3d4e40b8f202b4fa8c6ae49c6773
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-25 14:38:30 +0500

    the task was from 11.16-11.20

--- unions/solutions.cpp
+++ unions/solutions.cpp
@@ -19,6 +19,15 @@ int main()
         t1[i].letter = 'A';
         t1[i].real = 10;
     }
+
+        for (int i = 0; i < size; i++)
+    {
+        cout<<t1[i].whole<<endl;
+        cout<<t1[i].letter<<endl;
+        cout<<t1[i].real<<endl; 
+    }
     
     return 0;
 }
+
+// the answer will show 0 and 10 only because unions overwrite the previous data because of storing all the memory address as the largest of its member

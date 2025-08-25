bb1f58e5526e8824222433529bfd816fb7f7af0f
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-25 15:06:24 +0500

    task 11.22 done

--- enumerated_data_types/solutions.cpp
+++ enumerated_data_types/solutions.cpp
@@ -3,7 +3,16 @@ using namespace std;
 
 enum Flower { ROSE, DAISY, PETUNIA };
 // it will display 0,1,2 in memory
+
+enum { HOBBIT, ELF = 7, DRAGON };
 int main(){
     // cout<<ROSE;
+    cout << HOBBIT << " " << ELF << " " << DRAGON << endl;
+    /*
+    It will display these values as they are changed
+    HOBBIT-->0
+    ELF-->7
+    DRAGON-->0
+    */
     return 0;
 }

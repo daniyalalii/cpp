93df76688b64158277a4f196e00a374b3ceaccf3
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-25 18:07:34 +0500

    added solution for question number 3

---
+++ Programming_Challenges/Q3/solution.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+const int TotalMonths = 12;
+
+struct WeatherData
+{
+    double TotalRainfall;
+    double HighTemperature;
+    double LowTemperature;
+    double AvgTemperature;
+};
+
+void inputData(WeatherData d[])
+{
+    for (int i = 0; i < TotalMonths; i++)
+    {
+        cout << "Enter data for month: " << i + 1 << endl;
+        cout << "Enter total rainfall: " << endl;
+        cin >> d[i].TotalRainfall;
+        do
+        {
+            cout << "Enter Highest temperature: (-100 to +140) fahrenheit" << endl;
+            cin >> d[i].HighTemperature;
+        } while (d[i].HighTemperature > 140);
+        do
+        {
+            cout << "Enter Lowest Temperature: " << endl;
+            cin >> d[i].LowTemperature;
+        } while (d[i].LowTemperature < -100);
+        d[i].AvgTemperature = (d[i].HighTemperature + d[i].LowTemperature) / 2.0;
+    }
+}
+
+void showData(WeatherData d[])
+{
+    for (int i = 0; i < TotalMonths; i++)
+    {
+        cout << "------------" << endl;
+        cout << "Month " << i + 1 << ": "
+             << "Rainfall=" << d[i].TotalRainfall << " | "
+             << "High=" << d[i].HighTemperature << " | "
+             << "Low=" << d[i].LowTemperature << " | "
+             << "Avg=" << d[i].AvgTemperature << endl;
+        cout << "------------" << endl;
+    }
+}
+
+int main()
+{
+    WeatherData data[TotalMonths];
+    inputData(data);
+    showData(data);
+    return 0;
+}

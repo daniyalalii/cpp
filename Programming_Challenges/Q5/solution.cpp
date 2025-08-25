026397988cbc756e3a3187f48d6fdcaa16509fe4
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-25 21:23:11 +0500

    updated solution of question 5

--- Programming_Challenges/Q5/solution.cpp
+++ Programming_Challenges/Q5/solution.cpp
@@ -2,24 +2,6 @@
 #include <iomanip>
 using namespace std;
 
-/*
-## Monthly Budget
-### A student has established the following monthly budget:
-```
-Housing 500.00
-Utilities 150.00
-Household Expenses 65.00
-Transportation 50.00
-Food 250.00
-Medical 30.00
-Insurance 100.00
-Entertainment 150.00
-Clothing 75.00
-Miscellaneous 50.00
-```
-### Write a program that has a MonthlyBudget structure designed to hold each of these expense categories. The program should pass the structure to a function that asks the user to enter the amounts spent in each budget category during a month. The program should then pass the structure to a function that displays a report indicating the amount over or under in each category, as well as the amount over or under for the entire monthly budget.
-*/
-
 struct MonthlyBudget
 {
     double housing = 500.00;

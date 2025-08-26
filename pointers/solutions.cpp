209b0ddc0d9948a5bc05f1df752a970b215a09d0
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-26 20:37:36 +0500

    solution for task 9.3 added

--- pointers/solutions.cpp
+++ pointers/solutions.cpp
@@ -14,3 +14,17 @@ int main()
     cout<<"Value inside float ptr: "<<*fltptr<<endl;
     return 0;
 }
+
+/*
+List three uses of the * symbol in C++
+1- used in multiplying the numbers: 
+    2*2 = 4
+2- use for pointers to point a variable's memory address:
+    int num;
+    int *ptr = & num;
+3- use for derefrencing the value inside pointer;
+    (like in above case if num contains value 10, then)
+    num = 10;
+    cout<<*ptr;
+    it will print 10, the value of num, as ptr points to address of num.    
+*/

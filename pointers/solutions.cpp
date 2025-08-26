2b735b246c09f7f396a9d3ab7e3393434bbd2615
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-26 20:33:17 +0500

    solution for task 9.2 added

--- pointers/solutions.cpp
+++ pointers/solutions.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 int main()
 {
-    int count;
-    count = 10;
-    cout<<"Address of count is: "<<&count<<endl;
-    int *ptr = &count;
-    cout<<"Address using pointer: "<<ptr;
+    // int count;
+    // count = 10;
+    // cout<<"Address of count is: "<<&count<<endl;
+    // int *ptr = &count;
+    // cout<<"Address using pointer: "<<ptr;
+
+    float number = 10.2;
+    float *fltptr = &number;
+    cout<<"Value inside float ptr: "<<*fltptr<<endl;
     return 0;
 }

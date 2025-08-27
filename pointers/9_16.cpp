598946501789d6b219a77c41074ad6a61d861812
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-27 10:16:37 +0500

    solution added for task 9.17

--- pointers/9_16.cpp
+++ pointers/9_16.cpp
@@ -1,13 +1,32 @@
+// #include<iostream>
+// using namespace std;
+// void *pointerReturn(int *);
+// int main(){
+//     int value = 10;
+//     pointerReturn(&value);
+//     cout<<"After Function call: "<<value<<endl;
+//     return 0;
+// }
+// void *pointerReturn(int *ptr){
+//     *ptr = 12;
+//     return ptr;
+// }
+
+
+//-----------------------------
+//9.17
+
 #include<iostream>
 using namespace std;
-void *pointerReturn(int *);
+void *pointerReturn(int );
 int main(){
     int value = 10;
-    pointerReturn(&value);
+    // pointerReturn(&value);
     cout<<"After Function call: "<<value<<endl;
     return 0;
 }
-void *pointerReturn(int *ptr){
-    *ptr = 12;
-    return ptr;
+void *pointerReturn(int ptr){
+    ptr = 12;
+    // return ptr; 
 }
+// this is the wrong way and it will give error

a3fbc1903891d8d4b7d510229f913ec8d032363b
Daniyal Ali <daniyaljatt275@gmail.com>
2025-08-31 11:19:18 +0500

    added optimized solution for bubble sort

--- searching_and_sorting/sorting_algorithms/bubble_sort.cpp
+++ searching_and_sorting/sorting_algorithms/bubble_sort.cpp
@@ -7,7 +7,7 @@ void ShowOutput(int[], int);
 int main()
 {
     const int size = 10;
-    int arr[size] = {10, 3, 2, 4, 6, 1, 5, 7, 9, 8};
+    int arr[size] = {10, 2, 2, 4, 6, 1, 5, 7, 9, 8};
     cout << "Before Bubble Sort: " << endl;
     ShowOutput(arr, size);
     BubbleSort(arr, size);
@@ -27,16 +27,21 @@ void ShowOutput(int arr[], int size)
 
 void BubbleSort(int arr[], int size)
 {
+    bool swapped = false;
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
-                int temp = arr[j+1];
-                arr[j+1] = arr[j];
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
                 arr[j] = temp;
+                swapped = true;
             }
+        }
+        if(!swapped){
+            break;
         }
     }
 }

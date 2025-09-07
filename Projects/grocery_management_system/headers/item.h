3c5f0dcd430526a3ebeadaf3f0fb17e0ed41ad84
Daniyal Ali <daniyaljatt275@gmail.com>
2025-09-07 13:29:44 +0500

    added header file about item class

---
+++ Projects/grocery_management_system/headers/item.h
@@ -0,0 +1,56 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <iostream>
+using namespace std;
+
+class item
+{
+private:
+    string name;
+    string barcode;
+    double price;
+    int quantity;
+
+public:
+    item(string n, string bar, double p, int q)
+    {
+        name = n;
+        barcode = bar;
+        price = p;
+        quantity = q;
+    }
+    void changeItemName(string newName)
+    {
+        name = newName;
+    }
+    void changeItemPrice(double newPrice)
+    {
+        price = newPrice;
+    }
+    void addQuantity(int qty)
+    {
+        quantity += quantity;
+    }
+    void printItemDetails() const
+    {
+        cout << "Item Name: " << name << endl;
+        cout << "Barcode: " << barcode << endl;
+        cout << "Item Price: " << price << endl;
+        cout << "Quantity in hands: " << quantity << endl;
+    }
+    bool operator==(item &other)
+    {
+        bool result = false;
+        if (barcode == other.barcode)
+        {
+            result = true;
+        }
+        return result;
+    }
+    item operator+(item &other)
+    {
+        quantity += other.quantity;
+    }
+    
+};
+#endif

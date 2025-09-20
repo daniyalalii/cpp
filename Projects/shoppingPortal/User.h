fd19550dc47dcf4ff2863f8c996c8635f92fef36
Daniyal Ali <daniyaljatt275@gmail.com>
2025-09-20 19:18:02 +0500

    added base class user class, for inheriting the other classes --> customer --> admin --> vendor

---
+++ Projects/shoppingPortal/User.h
@@ -0,0 +1,52 @@
+#ifndef USER_H
+#define USER_H
+#include "./loginSignup.h"
+
+class User : public LoginSignUpSystem
+{
+private:
+    int userID;
+    int phoneNumber;
+    string name;
+    string email;
+    string password;
+    string address;
+
+public:
+    User(int id = 0, int number = 0, string n = "", string em = "", string pass = "", string addr = "")
+    {
+        userID = id;
+        phoneNumber = number;
+        name = n;
+        email = em;
+        password = pass;
+        address = addr;
+    }
+    virtual void login(int choice)
+    {
+        if (choice == 1)
+        {
+            LoginSignUpSystem::setLogin(true);
+        }
+        else
+        {
+            cout << "Error occured!" << endl;
+        }
+    }
+    virtual void logout()
+    {
+        cout << "Logged out successfully!" << endl;
+    }
+    virtual void displayInfo()
+    {
+        cout << "==================================" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Email: " << email << endl;
+        cout << "Password: " << password << endl;
+        cout << "User Id: " << userID << endl;
+        cout << "Phone Number: " << endl;
+        cout << "Address: " << address << endl;
+    }
+};
+
+#endif

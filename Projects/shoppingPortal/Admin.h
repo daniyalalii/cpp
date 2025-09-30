6961ee635cf94633d2d86c906e90e7465f00ed47
Daniyal Ali <daniyaljatt275@gmail.com>
2025-09-30 09:01:34 +0500

    updated admin class

--- Projects/shoppingPortal/Admin.h
+++ Projects/shoppingPortal/Admin.h
@@ -34,6 +34,55 @@ public:
         cout << "Total Orders: " << orders.size() << endl;
         inventory.displayInventory();
     }
+    void adminResponse(){
+        Inventory inventory;
+        Product product;
+        vector<User> users;
+        vector<Order> orders;
+        cout<<"Enter your choice: "<<endl;
+        cout<<"0. Exit admin menu"<<endl;
+        cout<<"1. Add products"<<endl;
+        cout<<"2. Update products"<<endl;
+        cout<<"3. Delete products"<<endl;
+        cout<<"4. Manage users"<<endl;
+        cout<<"5. View reports"<<endl;
+        int choice;
+        cin>>choice;
+        switch(choice){
+            case 1:
+                // Add products
+                addProduct(inventory, product);
+                break;
+            case 2:
+                // Update products
+                cout<<"Enter product ID to update: ";
+                int productId;
+                cin>>productId;
+                updateProduct(inventory, productId);
+                break;
+            case 3:
+                // Delete products
+                cout<<"Enter product ID to delete: ";
+                int productId;
+                cin>>productId;
+                deleteProduct(inventory, productId);
+                break;
+            case 4:
+                // Manage users
+                manageUsers(users);
+                break;
+            case 5:
+                // View reports
+                viewReports(orders, inventory);
+                break;
+            case 0:
+                cout << "Exiting admin menu." << endl;
+                break;
+            default:
+                cout<<"Invalid choice"<<endl;
+                break;
+        }
+    }
 };
 
 #endif 
